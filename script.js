f60bc02470c14d717b0b3fd69961c79958429bb8
anurag-1234567 <anuragnk7@gmail.com>
2026-09-25 11:56:44 +0530

    message

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function login()
 {
+  username=uname.value;
+  pass=password.value;
+
+if(username=="" || pass=="" )
+{
+
+  alert("enter any values")
+    
+}
+
+else{
       window.location="/employee.html";
-    username=uname.value;
+    
     console.log(username);
     localStorage.setItem("USERNAME",username)
-    
+
+}
 }
